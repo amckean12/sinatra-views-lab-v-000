c060f397d24df121cc02c4318bbc468659f8d8b4
Alex McKean <amckean@me.com>
2018-04-06 16:12:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ class App < Sinatra::Base
 		erb :index
 	end
 
-  get '/hello' do 
+  get '/hello' do
     erb :hello
   end
 
-  get '/goodbye' do 
+  get '/goodbye' do
     erb :goodbye
   end
 
-  get '/date' do 
+  get '/date' do
     erb :date
   end   
 end
